456d2e1baebe2fcd90e26ae165e08325469b5988
zhuweiji <55840431+zhuweiji@users.noreply.github.com>
2020-03-30 01:23:22 +0800

    Create colourlist.py

---
+++ FP project/colourlist.py
@@ -0,0 +1,18 @@
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+magenta = (255, 0, 255)
+maroon = (128, 0, 0)
+olive = (128, 128, 0)
+lime = (0, 128, 0)
+purple = (128, 0, 128)
+teal = (0, 128, 128)
+navy = (0, 0, 128)
+silver = (192, 192, 192)
+gray = (128, 128, 128)
+black = (0, 0, 0)
+white = (255, 255, 255)
+colour_list = [red, green, blue, yellow, cyan, magenta, maroon, olive, lime, purple,
+               teal, navy, silver, gray, black, white]
